49eed6b6ae4f46d793bb442b82050fd19982d294
an1shdey <deyanish669@gmail.com>
2026-03-27 19:43:12 +0530

    problem detail page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Navigate, Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage';
 import DashboardPage from './pages/DashboardPage';
 import ProblemsPage from './pages/ProblemsPage';
+import ProblemPage from './pages/ProblemPage';
 import { Toaster } from 'react-hot-toast';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to="/dashboard" />} />
         <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to="/" />} />
         <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to="/" />} />
+        <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to="/" />} />
       </Routes>
 
       <Toaster/>
